ff1e0097241530be7476277c7fa453ba68d1d693
Omosalewa-Akinsete <shaleycoral@gmail.com>
2024-02-06 17:10:53 -0800

    Create BaseModel from dictionary

--- models/base_model.py
+++ models/base_model.py
@@ -6,11 +6,32 @@ from datetime import datetime
 
 class BaseModel:
     """Base class for other models with common attributes and methods."""
-    def __init__ = (self):
+    def __init__ = (self, *args, **kwargs):
         """Initialize a new instance of the BaseModel class."""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        Args:
+            *args: The variable-length argument list (not used in this implementation).
+            **kwargs: The variable-length keyword argument list.
+                If not emepty, each key is an attribute name, and each value is a corresponding value
+                'created_at' and 'updated_at' values are converted from strings to datetime objects.
+                'id' is set to a new UUID if not present.
+                '__class__' from kwargs is ignored
+
+        if kwargs:
+            self.id = kwargs.get('id', str(uuid.uuid4()))
+            self.created_at = datetime.strptime(kwargs['created_at'], "%Y-%m-%dT%H:%M:%S.%f")
+            self.updated_at = datetime.strptime(kwargs['updated_at'], kwargs['created_at']), "%Y-%m-%dT%H:%M:%S.%f")
+
+            kwargs.pop('__class__', None)
+            kwargs.pop('id', None)
+            kwargs.pop('created_at', None)
+            kwargs.pop('updated_at', None)
+
+            for key, value in kwargs.items():
+                setattr(self, key, value)
+        else:        
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def save(self):
         """Update the 'update_at' attribute with the current datetime."""
