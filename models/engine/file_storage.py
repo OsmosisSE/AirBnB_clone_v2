c0afe5ea6dda9a86297f40c4626744b6da4cd27a
Omosalewa-Akinsete <shaleycoral@gmail.com>
2024-02-08 06:10:55 -0800

    AirBnB_clone

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-"""This is a module that contains the FileStorage that serializes instances to a JSON file and deserializes JSON file to instances."""
+"""This is a module that contains the FileStorage that serializes instances to a JSON file and deserialize JSON file to instances."""
 
 import json
 import os.path
-from models.ase_model import BaseModel
+from models.base_model import BaseModel
 from models.user import User
 from models.state import State
 from models.city import City
@@ -12,22 +12,22 @@ from models.place import Place
 from models.review import Review
 
 class FileStorage:
-    """Class for serializing instances to a JSON file and deserializing from a JSON file."""
+    """Class for serializing instances to JSON file and deserializing from a JSON file."""
 
     __file_path = "file.json"
-    __objects = {"BaseModel": BaseModel, "User": User, "State": State, "City": City, "Amenity": Amenity, "Place": Place, "Review": Review}
+    __objects = {"BaseMode": BaseModel, "User": User, "State": State, "City": City, "Amenity": Amenity, "Place": Place, "Review": Review}
 
     def all(self):
-        """Return the dictionary conatining all stored objects."""
+        """Return the dictionary containing all stored objects."""
         return self.__objects
 
     def new(self, obj):
         """Adds a new object to the storage dictionary."""
-        key = "{}.{}".format(obj.__class__.__name__, obj.id)
+        key = "{].{}".format(obj.__class__.__name__, obj.id)
         self.__objects[key] = obj
 
     def save(self):
-        """Serialize __objects to the JSON file (__file_path)."""
+        """Serialize__objects to JSON file (__file_path)."""
         obj_dict = {key: obj.to_dict() for key, obj in self.__objects.items()}
         with open(self.__file_path, 'w') as file:
             json.dump(obj_dict, file)
