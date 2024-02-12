a1dac058842df9ab2f2d75672d7ae8df068f51fd
OsmosisSE <sirosmosis1@gmail.com>
2024-02-12 14:04:07 -0800

    airbnb console

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""This is a module that contains the FileStorage that serializes instances to a JSON file and deserialize JSON file to instances."""
+"""Defines the Filestorage Class."""
 
 import json
 from models.base_model import BaseModel
@@ -10,8 +10,9 @@ from models.amenity import Amenity
 from models.place import Place
 from models.review import Review
 
+
 class FileStorage:
-    """Class for serializing instances to JSON file and deserializing from a JSON file."""
+    """Class for serializing instances."""
 
     __file_path = "file.json"
     __objects = {
@@ -52,7 +53,7 @@ class FileStorage:
             with open(FileStorage.__file_path) as f:
                 objdict = json.load(f)
                 for o in objdict.values():
-                    cls_name =o["__class__"]
+                    cls_name = o["__class__"]
                     del o["__class__"]
                     self.new(eval(cls_name)(**o))
         except FileNotFoundError:
